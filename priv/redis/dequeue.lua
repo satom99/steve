bdd595954a5e523f66e2fd328dc3c623cde603ec
satom99 <santitm99@gmail.com>
2018-10-22 00:04:27 +0200

    Typo

--- priv/redis/dequeue.lua
+++ priv/redis/dequeue.lua
@@ -3,11 +3,11 @@ local array = redis.call("ZRANGEBYSCORE", KEYS[1], 0, KEYS[2], "limit", 0, ARGV[
 for i,job in ipairs(array) do
   local hash = string.format("steve:jobs:%s", job)
   local values = redis.call("HMGET", hash, "queue", "content")
-  local desination = string.format("steve:%s:running:%s", values[1], KEYS[3])
+  local destination = string.format("steve:%s:running:%s", values[1], KEYS[3])
 
   redis.call("ZREM", KEYS[1], job)
   redis.call("HINCRBY", hash, "retry", 1)
-  redis.call("LPUSH", desination, job)
+  redis.call("LPUSH", destination, job)
 
   array[i] = values[2]
 end
--- priv/redis/recover.lua
+++ priv/redis/recover.lua
@@ -3,11 +3,11 @@ local array = redis.call("LRANGE", KEYS[1], 0, -1)
 for _,job in ipairs(array) do
   local hash = string.format("steve:jobs:%s", job)
   local queue = redis.call("HGET", hash, "queue")
-  local desination = string.format("steve:%s:queued", queue)
+  local destination = string.format("steve:%s:queued", queue)
 
   redis.call("ZREM", KEYS[1], job)
   redis.call("HINCRBY", hash, "retry", -1)
-  redis.call("LPUSH", desination, job)
+  redis.call("LPUSH", destination, job)
 end
 
 return #array
