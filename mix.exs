8521fa3a72cce37c975d3cbf23be4f2300def034
Santiago Tortosa <santiago.tortosa@azertiumit.com>
2019-08-20 13:35:14 +0200

    Reintroduce removed docs config

--- mix.exs
+++ mix.exs
@@ -8,7 +8,8 @@ defmodule Steve.MixProject do
       elixir: "~> 1.9",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs()
     ]
   end
 
@@ -25,6 +26,18 @@ defmodule Steve.MixProject do
       {:ecto_sql, "~> 3.1"},
       {:poolboy, "~> 1.5"},
       {:ex_doc, "~> 0.21.1", only: :dev}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "overview",
+      extras: [
+        "docs/Overview.md"
+      ],
+      groups_for_extras: [
+        "Introduction": ~r/docs\/.?/
+      ]
     ]
   end
 end
